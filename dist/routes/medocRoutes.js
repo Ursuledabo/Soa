bc0589d3edcb9e21d957b39ea533db90994a73e2
Sedra_Rabe <94762366+Sesedra@users.noreply.github.com>
2022-10-21 22:11:11 +0300

    Move files to server directory

--- dist/routes/medocRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const medocControllers_1 = require("../controllers/medocControllers");
-const router = (0, express_1.Router)();
-router.post("/add:pharmacyId", medocControllers_1.testAdd100RandomMedoc);
-router.get("/findOne:medocId", medocControllers_1.getMedoc);
-exports.default = router;
